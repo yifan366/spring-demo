ee2b146d643a328f91246d9a39e16819fc6c97c8
zhangfan <408250214@qq.com>
2016-10-09 18:02:47 +0800

    spring 学习

--- src/main/java/com/zhongtai/spring_demo/annotation/Main.java
+++ src/main/java/com/zhongtai/spring_demo/annotation/Main.java
@@ -5,6 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
+	
+	
+	
 	public static void main(String[] args) {
 		 ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-annotation.xml");
 		 
